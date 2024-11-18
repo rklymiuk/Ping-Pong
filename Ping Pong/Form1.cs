b83413fd9ea8355cc7f5a4b11e8143091baa7eeb
rklymiuk <klymrom05@gmail.com>
2024-11-18 18:52:26 +0200

    form maded, func names, variables

--- Ping Pong/Form1.cs
+++ Ping Pong/Form1.cs
@@ -2,6 +2,20 @@ namespace Ping_Pong
 {
     public partial class Form1 : Form
     {
+        int ballSpeedX = 4;
+        int ballSpeedY = 4;
+        int speed = 2;
+
+        Random rand = new Random();
+        bool goDown, goUp;
+        int computerSpeedChange = 50;
+        int playerScore = 0;
+        int computersScore = 0;
+        int playerSpeed = 0;
+
+
+
+
         public Form1()
         {
             InitializeComponent();
@@ -18,6 +32,15 @@ namespace Ping_Pong
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void CheckCollision(PictureBox Player, PictureBox Computer, int offset)
+        {
+
+        }
+        private void GameOver(string message)
         {
 
         }
